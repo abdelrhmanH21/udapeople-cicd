d5429ee6937c2751fb739de3f7d303096be35f28
abdelrhmanH21 <abdohussen75@gmail.com>
2023-01-11 13:55:01 +0200

    Start

---
+++ backend/set_env.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./src/migrations/*{.ts,.js}
+export TYPEORM_HOST=<RDS Instance Endpoint>
+export TYPEORM_PORT=<RDS Instance Port>
+export TYPEORM_USERNAME=<RDS Instance Username>
+export TYPEORM_PASSWORD=<RDS Instance Password>
+export TYPEORM_DATABASE=<RDS Instance Database>
